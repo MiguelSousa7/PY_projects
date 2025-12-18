7b5780a43d68914d4b2fa364ec76d3023c9f96d8
MiguelSousa7 <miguelsousabrg@gmail.com>
2025-12-18 16:53:02 +0000

    count how long it took to guess

--- num_guesser.py
+++ num_guesser.py
@@ -1,8 +1,11 @@
 import random
+import time
 
 num = random.randrange(1, 101)
 
 user_guess = 0
+guesses = 0    
+start = time.perf_counter()
 
 while user_guess != num:
     user_guess = input("Insert number between 1 and 100: ")
@@ -14,10 +17,13 @@ while user_guess != num:
             print("Please enter a number between 1 and 100!\n")
         elif user_guess > num:
             print ("\nIt's a lower number!")
+            guesses+=1
         elif user_guess < num:
             print ("\nIt's a higher number!")
-        
+            guesses+=1
     else: 
         print("Please insert a number!\n")
-    
+
+end = time.perf_counter()
 print(f"\n----Good job! The number was {num}----")
+print(f"You took {guesses+1} atempts and {end - start:.2f} seconds to guess!")
