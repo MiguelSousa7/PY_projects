0b905dd2f53acb49b9cf30384dd211cc470a7c7e
MiguelSousa7 <miguelsousabrg@gmail.com>
2025-12-18 16:27:14 +0000

    user input verifications

--- num_guesser.py
+++ num_guesser.py
@@ -5,12 +5,20 @@ num = random.randrange(1, 101)
 user_guess = 0
 
 while user_guess != num:
-    user_guess = int(input("Insert number between 1 and 100: "))
+    user_guess = input("Insert number between 1 and 100: ")
 
-    if user_guess > num:
-        print ("\nIt's a lower number!")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+        if user_guess < 1 or user_guess > 100:
+            print("Please enter a number between 1 and 100!\n")
+        elif user_guess > num:
+            print ("\nIt's a lower number!")
+        elif user_guess < num:
+            print ("\nIt's a higher number!")
+        
+    else: 
+        print("Please insert a number!\n")
+        continue
     
-    elif user_guess < num:
-        print ("\nIt's a higher number!")
-    
-print(f"\n---Good job! The number was {num}---")
+print(f"\n----Good job! The number was {num}----")
