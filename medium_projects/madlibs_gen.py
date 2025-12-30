211685e90912e820b5c0a05f89bf5ec460f927f7
MiguelSousa7 <miguelsousabrg@gmail.com>
2025-12-30 16:28:52 +0000

    madlib story game finished

--- medium_projects/madlibs_gen.py
+++ medium_projects/madlibs_gen.py
@@ -1,7 +1,7 @@
 with open("story.txt", "r") as f:
     story = f.read()
 
-words = []
+words = set()
 start_of_word = -1
 
 target_start = "<"
@@ -13,7 +13,15 @@ for i, char in enumerate(story):
     
     if char == target_end and start_of_word != -1:
         word = story[start_of_word : i + 1]
-        words.append(word)
+        words.add(word)
         start_of_word = -1
 
-print(words)
+answers = {}
+
+for word in words:
+    answers[word] = input(f"Entera word for {word}: ")
+
+for word in words:
+    story = story.replace(word, answers[word])
+
+print(story)
